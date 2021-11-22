3f13c7a856fa437421f455e40fb2fdd1d34369a8
Achasan <79975278+Achasan@users.noreply.github.com>
2021-11-22 11:04:31 +0900

    user regist action

---
+++ src/com/kurly/action/UserRegistAction.java
@@ -0,0 +1,21 @@
+package com.kurly.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kurly.controller.Action;
+import com.kurly.controller.ActionForward;
+
+public class UserRegistAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		
+		
+		return null;
+	}
+	
+}
