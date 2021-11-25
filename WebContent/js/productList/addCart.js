a6edb338e723f659bc105258fdd2475dea0b52cf
Achasan <79975278+Achasan@users.noreply.github.com>
2021-11-25 12:04:49 +0900

    장바구니 기능 추가(quantity)

--- WebContent/js/productList/addCart.js
+++ WebContent/js/productList/addCart.js
@@ -2,28 +2,30 @@ const cart_btns = document.getElementsByClassName('btn_cart');
 
 const addCart = (e) => {
 	const parent = e.target.parentNode;
-	const p_num = parent.previousSibling.previousSibling.value;
+	const p_num = parent.previousSibling.previousSibling;
 
 	$.ajax({
 		type: "post",
 		url: getContextPath() + "/addcart_list",
 		async: true,
-		data: { "p_num" : p_num },
+		timeout: 3000,
+		data: { "p_num" : p_num.value },
 		dataType: "text",
 		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
 		success: function(isAdded) {
 			if(isAdded === "true") {
 				alert('장바구니에 상품이 추가되었습니다.');
 				// 추가되었다는 메시지 출력, 모달?
+				
 			} else if(isAdded === "request login"){
-				alert('로그인이 필요한 기능입니다.');
-				location.href = getContextPath() + "/user/user_login.jsp";
+					alert('로그인이 필요한 기능입니다.');
+					location.href = getContextPath() + "/user/user_login.jsp";
 			} else {
-				alert('CartDTO 오류 발생, 확인필요');
+					alert('CartDTO 오류 발생, 확인필요');
 			}
 		},
 		error: function() {
-			alert('서버 통신에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+			alert('일시적으로 요청이 많아 장바구니 추가에 실패하였습니다. 잠시후 다시 시도해주세요.');
 		}
 	});
 };
@@ -31,6 +33,7 @@ const addCart = (e) => {
 for(element of cart_btns) {
 	element.addEventListener('click', addCart);
 }
+
 
 // js로 contextPath 구하기
 const getContextPath = () => {
